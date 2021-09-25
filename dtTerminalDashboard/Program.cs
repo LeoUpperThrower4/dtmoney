d712164edf23bc5d40d3463280858c98351cb4fd
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-09-25 09:23:27 -0300

    Add project files.

---
+++ dtTerminalDashboard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dtTerminalDashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
