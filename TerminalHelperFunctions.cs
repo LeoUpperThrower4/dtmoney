0ded72131a04bec76370bb2ea0f6345a15f05d9b
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-09-25 12:46:16 -0300

    add: created helper functions for terminal UI

---
+++ TerminalHelperFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class TerminalHelperFuncions
+{
+	public static string AskUser(string msg, bool sameLine = true)
+    {
+        if (sameLine)
+        {
+            Console.Write(msg); 
+        } 
+        else
+        {
+            Console.WriteLine(msg);
+        }
+        Console.ReadLine();
+    }
+}
