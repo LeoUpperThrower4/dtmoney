216b9452ae8c0db5892486e9e70d66c26f2024be
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-09-25 12:44:47 -0300

    add: created operations project

---
+++ dtOperations/dtOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace dtOperations
+{
+    public class Transaction
+    {
+        private float _amount;
+        private string _catergory;
+        private string _title;
+        private TransactionType _type;
+        public Transaction(float amount, string category, string title, TransactionType type)
+        {
+            _amount = amount;
+            _catergory = category;
+            _title = title;
+            _type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Título: {_title}\nValor: {_amount}\nTipo: {_type}";
+        }
+        public TransactionType GetTransactionType()
+        {
+            return _type;
+        }
+        public float GetTransactionAmount()
+        {
+            return _amount;
+        }
+    }
+    public enum TransactionType
+    {
+        Credit,
+        Debit
+    }
+}
