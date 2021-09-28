b7d8e3b77d987d43317c17692fd3d1c757ae877e
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2021-09-28 18:20:24 -0300

    updated NewTransactionModal

--- web/src/components/NewTransactionModal/index.tsx
+++ web/src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,30 @@
+import { MouseEventHandler } from 'react'
+import { FiArrowUpCircle, FiArrowDownCircle } from 'react-icons/fi'
+
 import styles from './styles.module.scss'
 
 export function NewTransactionModal() {
 
+  function handleChangeTransactionType(e: any) {
+    console.log(e);
 
+  }
   return (
     <form className={styles.form}>
       <h1>Cadastrar transação</h1>
       <input type="text" name="Title" id="title" placeholder="Título da transação" />
       <input type="number" name="Amount" id="amount" placeholder="Valor da transação" />
-      <label htmlFor="type">Tipo de transação</label>
-      <input type="radio" name="Credit" id="type" placeholder="Crédito" />
-      <input type="radio" name="Debit" id="type" placeholder="Débito" />
-      <input type="text" name="Category" id="category" placeholder="Categoria da trnsação" />
+      <div className={styles.radio}>
+        <button onClick={handleChangeTransactionType}>
+          <FiArrowUpCircle color="#12A454" />
+          Entrada
+        </button>
+        <button onClick={handleChangeTransactionType}>
+          <FiArrowDownCircle color="#E52E4D" />
+          Saída
+        </button>
+      </div>
+      <input type="text" name="Category" id="category" placeholder="Categoria da transação" />
     </form>
   )
 }
